8376f471567acfd78c7089b684a3e4de1562028f
OctoHoon <sakonghoon@naver.com>
2023-08-23 02:28:40 +0900

    add CSFR setting, CORS_ALLOW

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^wah$x%+tun3&j!$*vnj(q=n4+ra&a@1j2qq5(fuxpugk2dwc!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://147.46.245.226:8001"]
+
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://147.46.245.226:8001"]
+
 
 ROOT_URLCONF = "config.urls"
 
