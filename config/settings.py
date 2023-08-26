15658d75b33b09d016963a828d53d3ae8ebf11ec
OctoHoon <sakonghoon@naver.com>
2023-08-26 22:18:33 +0900

    setting

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://147.46.245.226:8001",
 ]
 
 CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://147.46.245.226:8001"]
