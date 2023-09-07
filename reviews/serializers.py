c5d46b2f10bf3cbb603e3b90bd23ebbe1ebfd4ff
OctoHoon <sakonghoon@naver.com>
2023-09-07 12:26:53 +0900

    review reply shop avatar

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -14,6 +14,7 @@ class ReviewPhotoSerializer(serializers.ModelSerializer):
 class ReviewReplySerializer(serializers.ModelSerializer):
     shop_pk = serializers.SerializerMethodField()
     shop_name = serializers.SerializerMethodField()
+    shop_avatar = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
 
     class Meta:
@@ -22,6 +23,7 @@ class ReviewReplySerializer(serializers.ModelSerializer):
             "pk",
             "shop_pk",
             "shop_name",
+            "shop_avatar",
             "content",
             "created_at",
         )
@@ -31,6 +33,9 @@ class ReviewReplySerializer(serializers.ModelSerializer):
 
     def get_shop_name(self, reply):
         return reply.shop.shop_name
+
+    def get_shop_avatar(self, reply):
+        return reply.shop.avatar
 
     def get_created_at(self, review):
         return review.created_at.strftime("%Y-%m-%d")
