7e6a971004a7db061eff7f133c7b0fa2728a04a3
EC2 Default User <ec2-user@ip-172-31-38-36.ap-northeast-2.compute.internal>
2023-11-12 14:13:43 +0000

    Revert "deploy: add gunicorn & fix settings.py & set staticfiles"
    This reverts commit 86fb27e0dc2d11a6652f92667454432da4ce188e.

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = "django-insecure-^wah$x%+tun3&j!$*vnj(q=n4+ra&a@1j2qq5(fuxpugk2dwc!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -179,7 +179,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
