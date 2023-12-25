0a61adaac5cbcd247c27d3ad60c612ea57a15a85
OctoHoon <sakonghoon@naver.com>
2023-12-25 17:21:18 +0900

    refactor: simply move review related urls and views to Reviews app

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,20 @@ from . import views
 
 urlpatterns = [
     path("<int:pk>", views.ReviewDetails.as_view()),
+    path("products/<int:pk>", views.ProductReviews.as_view()),
+    path(
+        "products/<int:pk>/reviews/<int:review_pk>", views.ProductReviewDetail.as_view()
+    ),
+    path(
+        "<int:review_pk>/reply",
+        views.ProductReviewResponse.as_view(),
+    ),
+    path("<int:product_pk>/reviews/images", views.ReviewImageList.as_view()),
+    # SHOP
+    path("<int:pk>/reviews", views.ShopReviews.as_view(), name="shop_reviews"),
+    path(
+        "<int:pk>/reviews/images/<int:product_pk>",
+        views.ReviewPhotos.as_view(),
+        name="review_photos",
+    ),
 ]
