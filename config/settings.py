326d80c515ce8c7b114a3ca8e2474cbf521620f4
OctoHoon <sakonghoon@naver.com>
2024-01-02 13:47:37 +0900

    chore: Leave only INFO level logs

--- config/settings.py
+++ config/settings.py
@@ -215,7 +215,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "handlers": {
         "file": {
-            "level": "DEBUG",
+            "level": "INFO",
             "class": "logging.FileHandler",
             "filename": "django.log",
             "formatter": "verbose",
@@ -230,7 +230,7 @@ LOGGING = {
     "loggers": {
         "django": {
             "handlers": ["file"],
-            "level": "DEBUG",
+            "level": "INFO",
             "propagate": True,
         },
     },
